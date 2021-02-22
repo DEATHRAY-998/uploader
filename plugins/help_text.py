f786537327527015783a005eec871d602d89c34d
DEATHRAY-998 <73659489+DEATHRAY-998@users.noreply.github.com>
2021-02-22 15:38:39 +0530

    Update

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -13,10 +13,8 @@ import sqlite3
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # the secret configuration specific things
-if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
-    from config import Config
+from sample_config import Config
+
 
 # the Strings used for this "thing"
 from translation import Translation
